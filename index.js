985c9b056eec0bb9facb45db4e53e230393b4eb1
PrabuddhaPal <94966905+prabuddha34@users.noreply.github.com>
2025-06-09 17:35:39 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const name=document.getElementById("name");
+const button=document.getElementById("button");
+
+<script src="https://cdn.jsdelivr.net/npm/qrcodejs@1.0.0/qrcode.min.js"></script>
+
+
+function generate(){
+const output=document.getElementById("output");
+const name=document.getElementById("name").value;
+}
